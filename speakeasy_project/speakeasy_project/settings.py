c7aa906ae5a5db3eaa5b3163370b796c6e52ad05
Alex Shved <alex@Alexs-MacBook-Air.local>
2015-09-13 19:37:20 -0400

    changed database to use pythonanywhere setup

--- speakeasy_project/speakeasy_project/settings.py
+++ speakeasy_project/speakeasy_project/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'speakeasy_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MVO$speakeasy_db',
+        'USER': 'MVO',
+        'PASSWORD': 'speakapp123',
+        'HOST': 'mysql.server',
+        'PORT': '',
     }
 }
 
