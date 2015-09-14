df9b6e0a282477e779851282a6a2f6ac90fe578b
Alex Shved <alex@Alexs-MacBook-Air.local>
2015-09-13 20:08:03 -0400

    changed database back to internal

--- speakeasy_project/speakeasy_project/settings.py
+++ speakeasy_project/speakeasy_project/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'speakeasy_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MVO$speakeasy_db',
-        'USER': 'MVO',
-        'PASSWORD': 'speakapp123',
-        'HOST': 'mysql.server',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'MVO$speakeasy_db',
+#         'USER': 'MVO',
+#         'PASSWORD': 'speakapp123',
+#         'HOST': 'mysql.server',
+#         'PORT': '',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
